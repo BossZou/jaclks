1e7b3130fa799428b333487b0b07b75eff2ce8f4
BossZou <yinghaozou000@gmail.com>
2025-03-30 17:37:20 +0800

    Support Clang-format (#3)
    * Add clang-format
    
    * Enable clang-format

--- include/hash/hasher.h
+++ include/hash/hasher.h
@@ -6,19 +6,19 @@ namespace jaclks {
 
 template <typename T, typename = void>
 struct Hasher {
-std::size_t operator()(const T& x) const noexcept {
-return std::hash<T>{}(x);
-}
+  std::size_t operator()(const T &x) const noexcept {
+    return std::hash<T>{}(x);
+  }
 };
 
 template <typename T>
-struct Hasher<T*> {
-        std::size_t operator()(const T* x) const noexcept {
-            if (x == nullptr) {
-                return 0;
-            }
-            return std::hash<T>{}(*x);
-        }
-    };
+struct Hasher<T *> {
+  std::size_t operator()(const T *x) const noexcept {
+    if (x == nullptr) {
+      return 0;
+    }
+    return std::hash<T>{}(*x);
+  }
+};
 
-}
+}  // namespace jaclks
