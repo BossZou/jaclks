64a6b484ac90080b7bffe5bd2e226c1cd6dace7f
BossZou <yinghaozou000@gmail.com>
2025-03-30 21:27:52 +0800

    Add Comparator (#6)
    Merged from branch:compator

---
+++ include/function/compare.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <type_traits>
+
+#include "compile/type_traits.h"
+
+namespace jaclks {
+
+template <typename T, typename = void>
+struct Comparator {
+  int operator()(const T &a, const T &b) const noexcept {
+    if (a < b) {
+      return -1;
+    }
+
+    return a == b ? 0 : 1;
+  }
+};
+
+template <>
+struct Comparator<std::string> {
+  int operator()(const std::string &a, const std::string &b) const noexcept {
+    return a.compare(b);
+  }
+};
+
+template <>
+struct Comparator<std::string_view> {
+  int operator()(const std::string_view &a, const std::string_view &b) const
+      noexcept {
+    return a.compare(b);
+  }
+};
+
+template <typename T>
+struct Comparator<T *> {
+  int operator()(const T *a, const T *b) const noexcept {
+    if (a == b) {
+      return 0;
+    }
+
+    if (a == nullptr || b == nullptr) {
+      return a == nullptr ? -1 : 1;
+    }
+
+    return Comparator<T>{}(*a, *b);
+  }
+};
+
+template <typename T>
+struct Comparator<T, std::enable_if_t<is_smart_pointer_v<T>>> {
+  using Type = std::decay_t<typename T::element_type>;
+
+  std::size_t operator()(const T &a, const T &b) const noexcept {
+    if (a.get() == b.get()) {
+      return 0;
+    }
+
+    if (a == nullptr || b == nullptr) {
+      return a == nullptr ? -1 : 1;
+    }
+
+    return Comparator<Type>{}(*a, *b);
+  }
+};
+
+}  // namespace jaclks
--- include/hash/hasher.h
+++ include/hash/hasher.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <functional>
+#include <unordered_map>
+
+#include "compile/type_traits.h"
 
 namespace jaclks {
 
@@ -18,6 +21,18 @@ struct Hasher<T *> {
       return 0;
     }
     return std::hash<T>{}(*x);
+  }
+};
+
+template <typename T>
+struct Hasher<T, std::enable_if_t<is_smart_pointer_v<T>>> {
+  using Type = std::decay_t<typename T::element_type>;
+
+  std::size_t operator()(const T &x) const noexcept {
+    if (x == nullptr) {
+      return 0;
+    }
+    return std::hash<Type>{}(*x);
   }
 };
 
