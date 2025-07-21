0f62cde8f7c095e425c6b1516e37b9d8d416e0a7
zouyinghao <yinghaozou000@gmail.com>
2025-07-21 23:44:38 +0800

    format code

--- src/concurrent/thread.cc
+++ src/concurrent/thread.cc
@@ -3,34 +3,34 @@
 namespace jaclks {
 
 namespace {
-void* thread_call(void *arg) {
-    if (arg != nullptr) {
-        auto runner = reinterpret_cast<Thread::Runner *>(arg);
-        runner->Run();
+void *thread_call(void *arg) {
+  if (arg != nullptr) {
+    auto runner = reinterpret_cast<Thread::Runner *>(arg);
+    runner->Run();
 
-        delete runner;
-    }
+    delete runner;
+  }
 }
-}
+}  // namespace
 
 Thread::~Thread() {
-    if (runner_ != nullptr) {
-        delete runner_;
-        runner_ = nullptr;
-    }
+  if (runner_ != nullptr) {
+    delete runner_;
+    runner_ = nullptr;
+  }
 }
 
 int Thread::Start() {
-    auto ret = pthread_create(&tid_, nullptr, thread_call, runner_);
-    if (0 == ret) {
-        runner_ = nullptr;
-    }
+  auto ret = pthread_create(&tid_, nullptr, thread_call, runner_);
+  if (0 == ret) {
+    runner_ = nullptr;
+  }
 
-    return ret;
+  return ret;
 }
 
 int Thread::Join() {
-    return pthread_join(tid_, nullptr);
+  return pthread_join(tid_, nullptr);
 }
 
-}
+}  // namespace jaclks
