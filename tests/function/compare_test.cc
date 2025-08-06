8f920674079d4f44068c0b56457d5d0b7b9d15d7
BossZou <yinghaozou000@gmail.com>
2025-08-06 21:46:11 +0800

    Optimize more tests

--- tests/function/compare_test.cc
+++ tests/function/compare_test.cc
@@ -2,12 +2,32 @@
 
 #include <gtest/gtest.h>
 
+#include <limits>
+
 namespace jaclks {
 
 TEST(Compare, IntegerComparator) {
   ASSERT_EQ(0, Comparator<int>{}(100, 100));
   ASSERT_EQ(-1, Comparator<int>{}(100, 200));
   ASSERT_EQ(1, Comparator<int>{}(100, -10000));
+  ASSERT_EQ(-1,
+            Comparator<int>{}(std::numeric_limits<int>::min(),
+                              std::numeric_limits<int>::max()));
+  ASSERT_EQ(1,
+            Comparator<int>{}(std::numeric_limits<int>::max(),
+                              std::numeric_limits<int>::min()));
+}
+
+TEST(Compare, SizeTComparator) {
+  ASSERT_EQ(0, Comparator<std::size_t>{}(100UL, 100UL));
+  ASSERT_EQ(-1, Comparator<std::size_t>{}(100UL, 200UL));
+  ASSERT_EQ(-1, Comparator<std::size_t>{}(100UL, -10000UL));
+  ASSERT_EQ(-1,
+            Comparator<std::size_t>{}(std::numeric_limits<std::size_t>::min(),
+                                      std::numeric_limits<std::size_t>::max()));
+  ASSERT_EQ(1,
+            Comparator<std::size_t>{}(std::numeric_limits<std::size_t>::max(),
+                                      std::numeric_limits<std::size_t>::min()));
 }
 
 TEST(Compare, StringComparator) {
@@ -54,6 +74,10 @@ TEST(Compare, PointerComparator) {
   ASSERT_EQ(-1, Comparator<int *>{}(x.get(), y.get()));
   ASSERT_EQ(1,
             Comparator<int *>{}(x.get(), std::make_unique<int>(-10000).get()));
+
+  ASSERT_EQ(0, Comparator<int *>{}(nullptr, nullptr));
+  ASSERT_EQ(-1, Comparator<int *>{}(nullptr, y.get()));
+  ASSERT_EQ(1, Comparator<int *>{}(x.get(), nullptr));
 }
 
 TEST(Compare, SmartPointerComparator) {
@@ -76,6 +100,16 @@ TEST(Compare, SmartPointerComparator) {
     ASSERT_EQ(0, Comparator<SmartT>{}(x, x));
     ASSERT_EQ(-1, Comparator<SmartT>{}(x, y));
     ASSERT_EQ(1, Comparator<SmartT>{}(x, std::make_shared<int>(-10000)));
+  }
+  {
+    using SmartT = std::shared_ptr<int>;
+
+    auto x = std::make_shared<int>(100);
+    auto y = std::make_shared<int>(200);
+
+    ASSERT_EQ(0, Comparator<SmartT>{}(nullptr, nullptr));
+    ASSERT_EQ(-1, Comparator<SmartT>{}(nullptr, y));
+    ASSERT_EQ(1, Comparator<SmartT>{}(x, nullptr));
   }
 }
 
