dbf58979c3c3aeaf0b002b499ec0c4834c3dd964
BossZou <firstcamer.xj@gmail.com>
2025-08-10 12:24:43 +0800

    Revert thread change

--- src/concurrent/thread.cc
+++ src/concurrent/thread.cc
@@ -1,44 +1,13 @@
 #include "concurrent/thread.h"
 
-#include <cstdio>
-#include <memory>
-
 namespace jaclks {
 
 namespace {
 
 void *thread_call(void *arg) {
   if (arg != nullptr) {
-#if defined(__linux__)
-#define THREAD_NAME_SIZE (16)
-#else
-#warning "Other platform which name size limit may confuse"
-#define THREAD_NAME_SIZE (64)
-#endif
-    char tname[THREAD_NAME_SIZE];
-    auto tid = pthread_self();
-
-    pthread_getname_np(tid, tname, THREAD_NAME_SIZE);
-
-    auto runner = static_cast<Thread::Runner *>(arg);
-    auto defer = std::unique_ptr<Thread::Runner>(runner);
-    try {
-      runner->Run();
-    } catch (const std::exception &e) {
-      std::fprintf(stderr,
-                   "[Thread-%lu %s] terminate with exception: %s\n",
-                   tid,
-                   tname,
-                   e.what());
-      // FIXME(BossZou): Consider if need rethrow exception here.
-      // throw;
-    } catch (...) {
-      std::fprintf(stderr,
-                   "[Thread-%lu %s] terminate with unknown exception\n",
-                   tid,
-                   tname);
-      throw;
-    }
+    auto runner = reinterpret_cast<Thread::Runner *>(arg);
+    runner->Run();
   }
 
   return nullptr;
@@ -69,14 +38,7 @@ int Thread::Cancel() {
 }
 
 int Thread::Join() {
-  // TODO(BossZou): Here if runner_ is not null mean thread not start.
-  // Try use
-  if (runner_) {
-    delete runner_;
-    return 0;
-  } else {
-    return pthread_join(tid_, nullptr);
-  }
+  return pthread_join(tid_, nullptr);
 }
 
 }  // namespace jaclks
--- tests/concurrent/thread_test.cc
+++ tests/concurrent/thread_test.cc
@@ -2,13 +2,12 @@
 
 #include <gtest/gtest.h>
 
-#include <cerrno>
 #include <memory>
 #include <mutex>
 
 namespace jaclks {
 
-TEST(ThreadTest, Normal) {
+TEST(Thread, Normal) {
   auto func = [](int *a) { *a += 1; };
 
   int val = 10;
@@ -20,7 +19,7 @@ TEST(ThreadTest, Normal) {
   ASSERT_EQ(val, 11);
 }
 
-TEST(ThreadTest, NoParam) {
+TEST(Thread, NoParam) {
   auto int_a = 10;
   auto func = [&int_a]() { ++int_a; };
 
@@ -32,7 +31,7 @@ TEST(ThreadTest, NoParam) {
   ASSERT_EQ(int_a, 11);
 }
 
-TEST(ThreadTest, UniqPtrArg) {
+TEST(Thread, UinqPtrArg) {
   auto func = [](std::unique_ptr<int> a_p, int *r) {
     *a_p += 1;
     *r = *a_p;
@@ -48,71 +47,21 @@ TEST(ThreadTest, UniqPtrArg) {
   ASSERT_EQ(res, 11);
 }
 
-TEST(ThreadTest, ThrowRuntimeError) {
-  auto func = [](int *a) {
-    if (nullptr == a) {
-      throw std::runtime_error("null pointer");
-    }
-    *a += 1;
-  };
-
-  auto t = Thread{func, nullptr};
-
-  ASSERT_EQ(t.Start(), 0);
-  ASSERT_EQ(t.Join(), 0);
-}
-
-TEST(ThreadTest, ThrowStdException) {
-  auto func = [](int *a) {
-    if (nullptr == a) {
-      throw std::logic_error("null pointer");
-    }
-    *a += 1;
-  };
-
-  auto t = Thread{func, nullptr};
-
-  ASSERT_EQ(t.Start(), 0);
-  ASSERT_EQ(t.Join(), 0);
-}
-
-TEST(ThreadTest, Cancel) {
-  struct CancelArg {
-    explicit CancelArg(bool *_stat) : stat(_stat) {}
-
-    ~CancelArg() {
-      *stat = true;
-    }
-
-    bool *stat;
-  };
-
+TEST(Thread, Cancel) {
   bool called = false;
-  bool exited = false;
 
-  auto func = [](bool *is_called, std::unique_ptr<CancelArg> arg) {
-    (void)(arg.get());
+  auto func = [](bool *is_called) {
     sleep(3);
     *is_called = true;
   };
 
-  auto t = Thread{func, &called, std::make_unique<CancelArg>(&exited)};
+  auto t = Thread{func, &called};
 
   ASSERT_EQ(t.Start(), 0);
   sleep(1);
   ASSERT_EQ(t.Cancel(), 0);
 
   ASSERT_FALSE(called);
-  ASSERT_TRUE(exited);
-}
-
-TEST(ThreadTest, CancelBeforeStart) {
-  auto func = [](int *a) { *a += 1; };
-
-  int val = 10;
-  auto t = Thread{func, &val};
-
-  ASSERT_EQ(ESRCH, t.Cancel());
 }
 
 }  // namespace jaclks
