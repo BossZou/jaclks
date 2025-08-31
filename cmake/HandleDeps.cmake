66611b3cd71d1535d338df29d13f349bc84c11ad
BossZou <firstcamer.xj@gmail.com>
2025-08-31 14:58:10 +0800

    Link GTest::gtest

--- cmake/HandleDeps.cmake
+++ cmake/HandleDeps.cmake
@@ -8,31 +8,45 @@ find_package(Git REQUIRED)
 
 find_package(OpenSSL REQUIRED)
 if(OPENSSL_FOUND)
-    message(STATUS "OpenSSL version: ${OPENSSL_VERSION}")
-    message(STATUS "OpenSSL include dir: ${OPENSSL_INCLUDE_DIR}")
-    message(STATUS "OpenSSL libraries: ${OPENSSL_LIBRARIES}")
+  message(STATUS "OpenSSL version: ${OPENSSL_VERSION}")
+  message(STATUS "OpenSSL include dir: ${OPENSSL_INCLUDE_DIR}")
+  message(STATUS "OpenSSL libraries: ${OPENSSL_LIBRARIES}")
 else()
-    message(FATAL_ERROR "OpenSSL not found")
+  message(FATAL_ERROR "OpenSSL not found")
 endif()
 
-find_package(GTest REQUIRED)
-if(GTEST_FOUND)
-    message(STATUS "GTest version: ${GTEST_VERSION}")
-    message(STATUS "GTest include dir: ${GTEST_INCLUDE_DIR}")
-    message(STATUS "GTest libraries: ${GTEST_LIBRARIES}")
-else()
+if(ENABLE_GTEST)
+  find_package(GTest REQUIRED)
+  if(GTest_FOUND)
+    message(STATUS "GTest found: ${GTest_FOUND}")
+    message(STATUS "GTest version: ${GTest_VERSION}")
+
+    message(
+      STATUS
+        "GTest targets available: GTest::gtest, GTest::gtest_main, GTest::gmock, GTest::gmock_main"
+    )
+
+    if(DEFINED GTEST_INCLUDE_DIRS)
+      message(STATUS "GTest include dirs: ${GTEST_INCLUDE_DIRS}")
+    endif()
+
+    if(DEFINED GTEST_LIBRARIES)
+      message(STATUS "GTest libraries: ${GTEST_LIBRARIES}")
+    endif()
+  else()
     message(FATAL_ERROR "GTest not found")
+  endif()
 endif()
 
 set(MyThirdParty_FOUND TRUE)
 
 if(NOT TARGET MyThirdParty::All)
-    add_library(MyThirdParty::All INTERFACE IMPORTED)
+  add_library(MyThirdParty::All INTERFACE IMPORTED)
 
-    # 添加所有依赖
-    target_link_libraries(MyThirdParty::All INTERFACE OpenSSL::SSL
-            OpenSSL::Crypto)
+  # 添加所有依赖
+  target_link_libraries(MyThirdParty::All INTERFACE OpenSSL::SSL
+                                                    OpenSSL::Crypto)
 
-    # # 添加编译定义（如果需要） target_compile_definitions(MyThirdParty::All INTERFACE
-    # USE_OPENSSL=1 USE_BOOST=1 )
+  # # 添加编译定义（如果需要） target_compile_definitions(MyThirdParty::All INTERFACE
+  # USE_OPENSSL=1 USE_BOOST=1 )
 endif()
