cf7560de55d6a9665588a9c7d79b4523dc9caed3
BossZou <firstcamer.xj@gmail.com>
2025-09-03 23:41:50 +0800

    Compitical for openssl 3.0 or newer

--- src/jaclks-internal/javac-base/java/security/md5_digest.cc
+++ src/jaclks-internal/javac-base/java/security/md5_digest.cc
@@ -6,6 +6,13 @@ MD5Digest::MD5Digest() : ctx_{} {
   md5_init();
 }
 
+MD5Digest::~MD5Digest() {
+#ifdef USE_EVP_MD5
+  EVP_MD_CTX_free(ctx_);
+  ctx_ = nullptr;
+#endif
+}
+
 void MD5Digest::EngineUpdate(const std::uint8_t *data, std::size_t num) {
   md5_update(data, num);
 }
@@ -20,19 +27,33 @@ void MD5Digest::EngineReset() {
 }
 
 void MD5Digest::md5_init() {
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+  ctx_evp_ = EVP_MD_CTX_new();
+  EVP_DigestInit_ex(ctx_evp_, EVP_md5(), nullptr);
+#else
   MD5_Init(&ctx_);
+#endif
 }
 
 void MD5Digest::md5_update(const std::uint8_t *data, std::size_t len) {
+#ifdef USE_EVP_MD5
+  EVP_DigestUpdate(ctx_, data, len);
+#else
   MD5_Update(&ctx_, data, len);
+#endif
 }
 
 String MD5Digest::md5_final() {
   unsigned char digest[MD5_DIGEST_LENGTH];
 
+#ifdef USE_EVP_MD5
+  unsigned int len;
+  EVP_DigestFinal_ex(ctx__, digest, &len);
+#else
   MD5_Final(digest, &ctx_);
+#endif
   return String{reinterpret_cast<const char *>(digest),
-                static_cast<std::size_t>(kMD5DigestLength)};
+                static_cast<std::size_t>(MD5_DIGEST_LENGTH)};
 }
 
 }  // namespace jaclks::javac_base
--- src/jaclks-internal/javac-base/java/security/md5_digest.h
+++ src/jaclks-internal/javac-base/java/security/md5_digest.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <openssl/md5.h>
+#include <openssl/opensslv.h>  // used for openssl version
 
 #include "jaclks-internal/javac-base/java/security/message_digest_spi.h"
 
 namespace jaclks::javac_base {
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+#define USE_EVP_MD5  // OpenSSL 3.0+ Use EVP API
+#endif
+
 class MD5Digest : public MessageDigestSpi {
  public:
   MD5Digest();
 
-  ~MD5Digest() override = default;
+  ~MD5Digest() override;
 
   void EngineUpdate(const std::uint8_t *data, std::size_t num) override;
 
@@ -19,17 +24,17 @@ class MD5Digest : public MessageDigestSpi {
   void EngineReset() override;
 
  private:
-  static constexpr int kMD5StateNum = 4;
-  static constexpr int kMD5BlockSize = 64;
-  static constexpr int kMD5DigestLength = 16;
-
   void md5_init();
 
   void md5_update(const std::uint8_t *data, std::size_t len);
 
   String md5_final();
 
+#ifdef USE_EVP_MD5
+  EVP_MD_CTX ctx_;
+#else
   MD5_CTX ctx_;
+#endif
 };
 
 }  // namespace jaclks::javac_base
