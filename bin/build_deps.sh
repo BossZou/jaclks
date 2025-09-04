f95c4a9736ba7b1fdf3ab357d4db5f20d76791ce
BossZou <firstcamer.xj@gmail.com>
2025-09-05 01:12:59 +0800

    Fix build deps err

--- bin/build_deps.sh
+++ bin/build_deps.sh
@@ -6,18 +6,18 @@ PROJECT_ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 THIRD_ROOT_DIR="$PROJECT_ROOT_DIR/thirdparty"
 THIRD_DIST_DIR="$PROJECT_ROOT_DIR/dist"
 
-pushd ${PROJECT_ROOT_DIR} >/dev/null
+pushd "${PROJECT_ROOT_DIR}" >/dev/null || exit
 
 ## Build googletest
 GOOGLETEST_ROOT_DIR="$THIRD_ROOT_DIR/googletest"
 
-cmake -B ${GOOGLETEST_ROOT_DIR}/build \
-	-S ${GOOGLETEST_ROOT_DIR} \
+cmake -B "${GOOGLETEST_ROOT_DIR}/build" \
+	-S "${GOOGLETEST_ROOT_DIR}" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_SHARED_LIBS=OFF \
 	-Dgtest_force_shared_crt=ON \
-	-DCMAKE_INSTALL_PREFIX=${THIRD_DIST_DIR}
+	-DCMAKE_INSTALL_PREFIX="${THIRD_DIST_DIR}"
 
-cmake --build ${GOOGLETEST_ROOT_DIR}/build --config Release --target install
+cmake --build "${GOOGLETEST_ROOT_DIR}/build" --config Release --target install
 
-popd >/dev/null
+popd >/dev/null || exit
