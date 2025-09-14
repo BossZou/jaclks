a760d4eb3e3cddcf20f09222f4a305aba7bd0710
BossZou <firstcamer.xj@gmail.com>
2025-09-14 21:09:12 +0800

    Equaler

--- include/jaclks/javac-base/java/util/hash_map.h
+++ include/jaclks/javac-base/java/util/hash_map.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "jaclks/javac-base/java/util/map.h"
 
-namespace jaclks::javac_base {}
+namespace jaclks::javac_base {
+
+template <typename K, typename V>
+class HashMap : Map<K, V> {
+private:
+  std::unordered_map<K, V> map_;
+};
+
+}
